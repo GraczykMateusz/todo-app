d4732fc246ad5e5b07b44ca09c54c76e10d0549a
Mateusz Graczyk <dev.graczykmateusz@gmail.com>
2023-04-03 18:55:46 +0200

    added components draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { TaskComponent } from './components/tasks-dashboard/task/task.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TasksDashboardComponent } from './components/tasks-dashboard/tasks-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    DashboardComponent,
+    TasksDashboardComponent
   ],
   imports: [
     BrowserModule,
